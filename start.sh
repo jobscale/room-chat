80714de939ecc9a7ad89200b1ae6d7453f3c1144
jobscale <jobscale@jsx.jp>
2018-12-14 13:48:42 +0900

    with supervisor

--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@ export PORT=3033
 export DB_HOST=db
 
 cd /var/site/projects/node/chat
-[[ -s "$HOME/.nvm/nvm.sh" ]] && . "$HOME/.nvm/nvm.sh"
-[[ -s "$HOME/.nvm/nvm.sh" ]] && nvm use v8.14.0
+[[ -s "/var/site/.nvm/nvm.sh" ]] && . "/var/site/.nvm/nvm.sh"
+[[ -s "/var/site/.nvm/nvm.sh" ]] && nvm use v8.14.0
 npm run inspect
