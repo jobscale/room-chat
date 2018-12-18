28a0f5ba7600e22bfb245930d6c240c1eb88dc6e
jobscale <jobscale@jsx.jp>
2018-12-18 09:49:51 +0900

    support supervisor

--- start.sh
+++ start.sh
@@ -2,7 +2,13 @@
 
 export PORT=3033
 export DB_HOST=db
+export CLUSTERING=false
 
 cd /var/site/projects/node/chat
-PATH="$PATH:/var/site/.nvm/versions/node/v8.14.0/bin"
+if [[ $(declare -F nvm >& /dev/null; echo $?) -eq '0' ]]
+then
+  nvm use v8.14.0
+else
+  PATH="$PATH:/var/site/.nvm/versions/node/v8.14.0/bin"
+fi
 npm run inspect
