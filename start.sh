bf653729332f0943949df88332774ffda3770470
jobscale <jobscale@jsx.jp>
2019-04-27 08:57:36 +0900

    check ready redis

--- start.sh
+++ start.sh
@@ -1,8 +1,31 @@
 #!/bin/bash -eu
 
-export PORT=3033
+export DB_PORT=6379
 export DB_HOST=db
 #export CLUSTERING=true
+
+check() {
+  nc $DB_HOST $DB_PORT -w 1 < /dev/null
+  if [ $? -eq 0 ]
+  then
+    echo -e "\nOK!"
+    return 0
+  fi
+  return 1
+}
+
+waiter() {
+  while true
+  do
+    check
+    if [ $? -eq 0 ]
+    then
+      break
+    fi
+    sleep 60
+  done
+}
+waiter
 
 . /home/jobscale/.nvm/nvm.sh
 cd /var/site/projects/node/chat
